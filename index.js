5bf2c79ddb614113f6b5238be4d2ddf23d441d68
EstudanteMariaCarol <maria.fracasso@estudante.ifms.edu.br>
2022-08-17 13:54:52 -0400

    Novas Alterações

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { usuario } = require('./models');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.get('/', async function(req, res){
+//  var resultado = await usuario.findAll();
+//  res.json(resultado);
+});
+
+app.get('/autenticar', async function(req, res){
+  res.render("autenticar")
+  });
+
+app.listen(3000, function() {
+  console.log('App de Exemplo escutando na porta 3000!')
+});
